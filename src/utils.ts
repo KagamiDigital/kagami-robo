47a63f8f013d3f06a4f3b98a61cd2443ac5ab92e
josephbouassaf <joseph.bouassaf@mail.mcgill.ca>
2024-09-30 21:35:42 -0400

    added sepolia base

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,8 @@ export function getRPCNodeFromNetworkId(networkId:string) {
         return process.env.AMOY_NODE_URL; 
     } else if(networkId === '1287') {
         return process.env.MOONBASE_NODE_URL; 
+    } else if(networkId === '84532') {
+        return process.env.BASE_SEPOLIA_NODE_URL; 
     } else {
         return ''; 
     }
