32acd2cf90863cd56c2ecba3e4197f13751ec0be
Patrick O'Dacre <patrick@patrickwho.me>
2024-11-07 17:25:46 -0500

    Add new script to get keypairs from seedphrase for testing

---
+++ scripts/seed_to_keypairs.js
@@ -0,0 +1,40 @@
+const { ethers } = require('ethers');
+
+function deriveAccounts(mnemonics, numAccounts = 10) {
+
+	for (const mnemonic of mnemonics) {
+
+	    const hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic);
+	    const accounts = [];
+
+	    for(let i = 0; i < numAccounts; i++) {
+	        const path = `m/44'/60'/0'/0/${i}`;
+	        const account = hdNode.derivePath(path);
+	        accounts.push({
+	        	mnemonic,
+	            index: i,
+	            path,
+	            address: account.address,
+	            publicKey: account.publicKey,
+	            privateKey: account.privateKey
+	        });
+	    }
+	}
+
+    return accounts;
+}
+
+// Usage:
+const mnemonics = [
+	"sentence liberty steel nephew current never exact damp staff work peanut street solution year movie call chalk grant unveil link upon evil faint unaware",
+	"law fatal shed clock cup pottery eight payment better belt bunker degree own senior spy odor sentence such wrestle market stool peace rail upset",
+	];
+const accounts = deriveAccounts(mnemonics);
+
+// Find matching accounts
+accounts.forEach(acc => {
+    console.log(`Index ${acc.index}:`);
+    console.log(`Address: ${acc.address}`);
+    console.log(`Private Key: ${acc.privateKey}`);
+    console.log('---');
+});
