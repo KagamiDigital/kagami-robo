1bee5481ab7941a64854eb3bba5592a0a000df38
Patrick O'Dacre <patrick@patrickwho.me>
2024-11-14 16:21:58 -0500

    Update robonet generate keys script

--- scripts/generate_keypairs.js
+++ scripts/generate_keypairs.js
@@ -1,5 +1,6 @@
 const { ethers } = require('ethers');
 const { spawn } = require('child_process');
+const {io} = require('socket.io-client')
 const dotenv = require("dotenv")
 dotenv.config();
 const {
@@ -13,6 +14,9 @@ const crypto = require('crypto');
 const fs = require('fs').promises;
 
 // Environment configuration
+// new_keys || existing_keys
+const DEPLOYMENT_TYPE = process.env.DEPLOYMENT_TYPE || "new"
+const KEYS_JSON = process.env.KEYS_JSON || null
 const AWS_REGION = process.env.AWS_REGION || 'us-east-1';
 const NUM_KEYS_TO_GENERATE = parseInt(process.env.NUM_KEYS_TO_GENERATE || '1', 10);
 const LOG_SENSITIVE = process.env.LOG_SENSITIVE === 'true';
@@ -216,47 +220,166 @@ async function processKey(privateKeyHex) {
 // Main execution
 async function main() {
     try {
-        const results = [];
-        const keyIds = [];
-        const privateKeys = []
-        for (let i = 0; i < NUM_KEYS_TO_GENERATE; i++) {
 
-            const wallet = ethers.Wallet.fromMnemonic( ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32)) )
+        const socket = io(process.env.API_URL + "/robo", {
+            query: {
+                apiKey: process.env.API_KEY,
+            },
+            transports: ["websocket"]
+        });
 
+        socket.on("error", async (err) => {
+            console.log(`Robonet Error :: Connected to API Service...`, err)
             await logOperation({
-                event: 'mnemonic_phrase',
-                seedPhrase: wallet.mnemonic.phrase,
+                event: 'robonet_error',
+                error: err,
+            })
+        });
+
+        socket.on("connect_error", async (err) => {
+            console.log(`Robonet Connect Error :: Connected to API Service...`, err)
+            await logOperation({
+                event: 'robonet_connect_error',
+                error: err,
+            })
+        });
+
+        socket.on("connect", async () => {
+            console.log(`Robonet :: Connected to API Service...`)
+            await logOperation({
+                event: 'robonet_connected_to_api',
             })
 
-            const keyId = await processKey(wallet.privateKey);
+        })
 
-            keyIds.push(keyId);
-            privateKeys.push(wallet.privateKey)
+        socket.on("confirmed_robos_initialized", async () => {
 
-            results.push({
-                keyId,
-                ethereumAddress: wallet.address
-            });
-
+            console.log(`Robonet :: Robos already initialized`)
             await logOperation({
-                event: 'key_processed',
-                keyId,
-                ethereumAddress: wallet.address
-            });
-        }
+                event: 'robonet_already_initialized',
+            })
 
-        console.log('Processed Keys:', results);
+            process.exit(0);
+        })
 
-        // Append keyIds to .env file
-        const fs = require('fs');
-        const keyIdsString = `\nKMS_KEY_IDS="${keyIds.join(',')}"`
-        const publicKeysString = `\nPUBLIC_KEYS="${results.map(r => r.ethereumAddress).join(',')}"`
-        const privateKeysString = `\nKEYS="${privateKeys.map(k => k).join(',')}"`
+        // API did not find any existing roboSigners,
+        // so it will request the signers information from the Robo
+        socket.on("request_initialize_robos", async () => {
+            console.log("Received request from API to initialize robos")
 
-        fs.appendFileSync('.env', keyIdsString);
-        fs.appendFileSync('.env', publicKeysString);
-        fs.appendFileSync('.env', privateKeysString);
-        console.log('Added key IDs to .env file');
+            const results = [];
+            const keyIds = [];
+            const privateKeys = []
+
+
+            // Should we generate new keys, or are we deploying this robo with existing keys?
+            if ("import" === DEPLOYMENT_TYPE) {
+
+                if (!KEYS_JSON) {
+                    console.error("KEYS_JSON required for public / private key pairs")
+                    process.exit(1)
+                }
+
+                const keysJson = JSON.parse(KEYS_JSON)
+
+                const publicKeys = []
+                const privateKeys = []
+
+                for (const publicKey in keysJson) {
+                    const privateKey = keysJson[publicKey]
+
+                    publicKeys.push(publicKey)
+                    privateKeys.push(privateKey)
+
+                    socket.emit('robonet_wallet_created', {
+                        publicKey: publicKey,
+                        seedPhrase: "",
+                        source: "imported",
+                    })
+                }
+
+                const fs = require('fs');
+                const publicKeysString = `\nPUBLIC_KEYS="${publicKeys.join(',')}"`
+                const privateKeysString = `\nKEYS="${privateKeys.join(',')}"`
+
+                const timestamp = new Date().toISOString();
+                fs.appendFileSync('.env', "\n\n");
+                fs.appendFileSync('.env', "# ===== START ===== #\n");
+                fs.appendFileSync('.env', `# Keys IMPORTED at ${timestamp}`);
+                fs.appendFileSync('.env', publicKeysString);
+                fs.appendFileSync('.env', privateKeysString);
+                fs.appendFileSync('.env', "\n");
+                fs.appendFileSync('.env', "# ===== END ===== #");
+
+            } else if ("new" === DEPLOYMENT_TYPE) {
+
+                await logOperation({
+                    event: "new_key_deployment",
+                    num_of_keys: NUM_KEYS_TO_GENERATE,
+                })
+
+                for (let i = 0; i < NUM_KEYS_TO_GENERATE; i++) {
+
+                    const wallet = await ethers.Wallet.fromMnemonic( ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32)) )
+                    console.log("Wallet created :: ", wallet.address)
+
+                    socket.emit('robonet_wallet_created', {
+                        publicKey: wallet.address,
+                        seedPhrase: wallet.mnemonic.phrase,
+                        source: "created",
+                    })
+
+                    await logOperation({
+                        event: 'mnemonic_phrase',
+                        seedPhrase: wallet.mnemonic.phrase,
+                    })
+
+                    // import keys to AWS KMS
+                    // const keyId = await processKey(wallet.privateKey);
+                    // keyIds.push(keyId);
+
+                    privateKeys.push(wallet.privateKey)
+
+                    results.push({
+                        // keyId,
+                        ethereumAddress: wallet.address
+                    });
+
+                    await logOperation({
+                        event: 'key_processed',
+                        // keyId,
+                        ethereumAddress: wallet.address
+                    });
+                }
+
+                console.log('Processed Keys:', results);
+
+                // Append keyIds to .env file
+                const fs = require('fs');
+                const keyIdsString = `\nKMS_KEY_IDS="${keyIds.join(',')}"`
+                const publicKeysString = `\nPUBLIC_KEYS="${results.map(r => r.ethereumAddress).join(',')}"`
+                const privateKeysString = `\nKEYS="${privateKeys.join(',')}"`
+
+                fs.appendFileSync('.env', keyIdsString);
+                const timestamp = new Date().toISOString();
+                fs.appendFileSync('.env', "\n\n");
+                fs.appendFileSync('.env', "# ===== START ===== #\n");
+                fs.appendFileSync('.env', `# Keys GENERATED at ${timestamp}`);
+                fs.appendFileSync('.env', publicKeysString);
+                fs.appendFileSync('.env', privateKeysString);
+                fs.appendFileSync('.env', "\n");
+                fs.appendFileSync('.env', "# ===== END ===== #");
+
+                console.log('Added key IDs to .env file');
+
+                await logOperation({
+                    event: 'env_updated',
+                    message: "keys written to .env file"
+                });
+            }
+
+            process.exit(0);
+        })
 
     } catch (error) {
         console.error('Main process error:', error);
