032cedad2e778a3de6195d3be6e3285dae74866b
Edd <accounts@edd.fm>
2025-03-26 15:46:19 +0000

    feat: add somnia network support

--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,8 @@ export function getRPCNodeFromNetworkId(networkId:string) {
         return process.env.MOONBASE_NODE_URL; 
     } else if(networkId === '84532') {
         return process.env.BASE_SEPOLIA_NODE_URL; 
+    } else if (networkId === '50312') {
+        return process.env.SOMNIA_SHANNON_NODE_URL;
     } else {
         return ''; 
     }
