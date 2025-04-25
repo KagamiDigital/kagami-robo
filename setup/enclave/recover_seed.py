ab5ef74d0c6d6e8c8becf13aed2afd17fcf057ff
josephbouassaf <joseph.bouassaf@mail.mcgill.ca>
2025-04-25 15:52:56 -0600

    added missing recover file

---
+++ setup/enclave/recover_seed.py
@@ -0,0 +1,14 @@
+from decrypt import kms_decrypt
+
+def recover():
+    """Recover seedphrase using CMK
+
+    KeyId is used to identify the CMKs to use for decryption.
+    """
+    with open("/app/seed.txt", "rb") as file:
+        cyphertext = file.read()
+    with open("/app/keyId.txt", "r") as file:
+        keyId = file.read()
+    seed = kms_decrypt(cyphertext, keyId)
+    
+    return seed
