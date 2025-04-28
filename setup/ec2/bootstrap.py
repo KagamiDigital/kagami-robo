c03ba103ff4ad0aa2158f97fb237aeed110fabd9
josephbouassaf <joseph.bouassaf@mail.mcgill.ca>
2025-04-28 14:05:30 -0600

    removed vpc configuration and sleep code after kms key is generated to avoid errors

--- setup/ec2/bootstrap.py
+++ setup/ec2/bootstrap.py
@@ -4,6 +4,7 @@ from encrypt import kms_encrypt
 from ecies.utils import generate_eth_key
 from ecies import encrypt, decrypt
 import binascii
+import time
 
 keyId, keyARN = create_cmk("robo-enclave-key")
 
@@ -11,6 +12,8 @@ with open("keyId.txt", "w") as file:
     file.write(keyId)
 with open("keyARN.txt", "w") as file:
     file.write(keyARN)
+
+time.sleep(10)
 
 mnemo = Mnemonic("english")
 words = mnemo.generate(strength=256)
